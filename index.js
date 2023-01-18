1707b4615df250b4d0ea700971f437f621e459eb
peiciqiu <peiciqiu@seas.upenn.edu>
2023-01-18 18:53:44 -0500

    Typo fixes

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ prevBtn.forEach((button) => {
   });
 });
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const inputs = [];
-//   form.querySelectorAll("input").forEach((input) => {
-//     const { name, value } = input;
-//     inputs.push({ name, value });
-//   });
-//   console.log(inputs);
-//   form.reset();
-// });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputs = [];
+  form.querySelectorAll("input").forEach((input) => {
+    const { name, value } = input;
+    inputs.push({ name, value });
+  });
+  console.log(inputs);
+  form.reset();
+});
 
 function changePage(btn) {
   let index = 0;
